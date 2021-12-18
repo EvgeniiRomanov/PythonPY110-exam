2a2fd3808a12f1e336cbd0aa0c16ee0ad2073a7b
EvgeniiRomanov <e78rus@mail.ru>
2021-12-18 09:50:58 +0000

    Create func for title

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
 import random
 
 from string import digits
+from faker import Faker
 from conf import MODEL
 
+INPUT_FILE = "books.txt"
 
 def main():
 
@@ -16,12 +17,15 @@ def main():
 
 
 #-------------functions for Fields-------------------------------------------------
-        # 3.2 func  random generate fictional year
-        def read_book_name() -> str:
-            tmp_year = random.sample(digits, lenght_)
-            tmp_year_str = "".join(tmp_year)
+    # 3.1 func random read one book from file book.txt
+    def read_rows_from_book() -> str:
+        with open(INPUT_FILE, "r", encoding="UTF8") as f:
+            rows_ = f.readlines()
+            rows_ = [x.strip() for x in rows_]
+            rows_random = random.choice(rows_)
 
-            return tmp_year_str
+        return rows_random
+
 
     # 3.2 func  random generate fictional year
     def generate_random_year(lenght_= 4) -> int:
@@ -30,12 +34,18 @@ def main():
 
         return int(tmp_year_str)
 
+
     # 3.3 func  random generate pages
     def generate_random_pages(lenght_= 3) -> int:
         tmp_pages = random.sample(digits, lenght_)
         tmp_pages_str = "".join(tmp_pages)
 
         return int(tmp_pages_str)
+
+    # 3.4 func generate random isbn13
+
+
+
 
     # 3.4 func random generate "isbn13" from Faker
     # def generate_random_pages(lenght_= 3) -> str:
@@ -57,6 +67,11 @@ def main():
         tmp_price = round(random.uniform(100, 2000), 2)
 
         return tmp_price
+
+
+    # 3.7 func random generate price(float)
+    def generate_author():
+        pass
 
 if __name__ == '__main__':
     main()
--- test_tmp.py
+++ test_tmp.py
@@ -1,29 +1,23 @@
-from string import digits, ascii_letters
-import random
-from itertools import count
-#from traceback import print_exc
+from faker import Faker
 
-# def password_min_8symbols(lenght_ = 8):
+# def gen_isbn13():
+#     fake = Faker()
+#     Faker.seed(0)
+#     k = fake.isbn13()
 #
-#     #while True:
-#     #for _ in count(1):
-#     tmp_pwd = random.sample(digits+ascii_letters, lenght_)
-#     random.shuffle(tmp_pwd)
+#     return k
 #
-#     return "".join(tmp_pwd)
 #
-# print(password_min_8symbols())
+# print(gen_isbn13())
 
-from string import digits, ascii_letters
-import random
-from itertools import count
-
-
-def generate_random_pages(lenght_=3) -> str:
-    tmp_pages = random.sample(digits, lenght_)
-    tmp_pages_str = "".join(tmp_pages)
-
-    return int(tmp_pages_str)
-
-k = generate_random_pages()
-print(k, type(k))
+# print(gen_isbn13())
+sbn_ = []
+fake = Faker()
+Faker.seed(0)
+for _ in range(2):
+   print(fake.isbn13())
+   sbn_.append(_)
+print(sbn_)
+# k = fake.isbn13()
+# print(k)
+# print(k)
