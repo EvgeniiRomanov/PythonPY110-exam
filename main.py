a5fd08c7ea946a60735218db573226bb2f9714d6
EvgeniiRomanov <e78rus@mail.ru>
2021-12-18 14:07:34 +0000

    Финишная версия (выгрузка 30 сек)

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import random
 import json
 
-from string import digits
 from faker import Faker
-from traceback import print_exc
 from conf import MODEL
 
 INPUT_FILE = "books.txt"
 OUTPUT_FILE = "books_JSON.json"
 
 
-def main():
+def main() -> json:
 
     while True:
         try:
-            n = int(input("Введите стартовое число отсчета от 1 до 100 (по умолчанию 1): "))
-            if 1 <= n <= 100:
+            num_tmp = int(input("Введите стартовое число отсчета от 1 до 100 (по умолчанию 1): "))
+            if 1 <= num_tmp <= 100:
                 break
             else:
                 print(f"Вы ввели чмсло не из требуемого диапазона, попробуйте снова.")
@@ -23,31 +21,20 @@ def main():
         except ValueError:
             print("Вы ввели не число, попробуйте снова.")
 
-    start_pk = 1                            # начальное значение счетчика
-    if n >= start_pk:
-        start_pk = n
-        numbers_of_models = 10               # число выгружаемых записей
-        result_list = []                    # выходной список в json
+    start_pk = 1
+    if num_tmp >= start_pk:
+        start_pk = num_tmp
+        numbers_of_models = 100              # numbers of models load to json
+        result_list = []                   # list to json
 
-        # with open(OUTPUT_FILE, "w") as f:
-        #
-        #     for i in range(start_pk, numbers_of_models+1):
-        #         k_ = next(generator_library(i))
-        #         print(k_)
-        #         result_list.append(k_)
-        #         print(result_list)
-        #         json.dump(result_list, f, indent=4, ensure_ascii=False)
-
-        for i in range(start_pk, numbers_of_models+1):
-            k_ = next(generator_library(i))
+        for number_ in range(start_pk, numbers_of_models+1):
+            k_ = next(generator_library(number_))
             result_list.append(k_)
 
         with open(OUTPUT_FILE, "w") as f:
             json.dump(result_list, f, indent=4, ensure_ascii=False)
 
-#----------------------------------------------------------------------------------
 
-    #Func generator
 def generator_library(count_ = 1) -> dict:
 
     all_model_ = {
@@ -66,9 +53,9 @@ def generator_library(count_ = 1) -> dict:
 
     yield all_model_
 
-#-------------functions for Fields-------------------------------------------------
-    # 3.1 func random read one book from file book.txt
+
 def read_random_bookname_fromfile() -> str:
+
     with open(INPUT_FILE, "r", encoding="UTF8") as f:
         rows_ = f.readlines()
         rows_ = [x.strip() for x in rows_]
@@ -77,57 +64,40 @@ def read_random_bookname_fromfile() -> str:
     return rows_random
 
 
-    # 3.2 func  random generate fictional year
-def generate_random_year(lenght_= 4) -> int:
-    tmp_year = random.sample(digits, lenght_)
-    tmp_year_str = "".join(tmp_year)
-
-    return int(tmp_year_str)
+def generate_random_year() -> int:
+    tmp_year = random.randint(1800, 2100)
+    return tmp_year
 
 
-    # 3.3 func  random generate pages
-def generate_random_pages(lenght_= 3) -> int:
-    tmp_pages = random.sample(digits, lenght_)
-    tmp_pages_str = "".join(tmp_pages)
-
-    return int(tmp_pages_str)
+def generate_random_pages() -> int:
+    tmp_pages = random.randint(100, 1000)
+    return tmp_pages
 
 
-    # 3.4 func generate random isbn13
 def generate_random_isbn13() -> str:
     fake = Faker()
     isbn13_ = fake.isbn13()
-
     return isbn13_
 
 
-    # 3.5 func random generate raiting (float)
 def generate_random_rating() -> float:
-        #tmp_taring = random.randint(0, 5) + random.random()
     tmp_rating = round(random.uniform(0, 5), 7)
-
     return tmp_rating
 
 
-    # 3.6 func random generate price(float)
 def generate_random_price() -> float:
-        #tmp_taring = random.randint(0, 5) + random.random()
     tmp_price = round(random.uniform(100, 2000), 2)
-
     return tmp_price
 
 
-    # 3.7 func random generate from 1 to 3 authors
 def generate_random_author() -> list:
     name_author = []
     count_ = random.randint(1, 3)
     for i in range(count_):
-            fake = Faker()
-            name_author.append(fake.name())
+        fake = Faker()
+        name_author.append(fake.name())
 
     return name_author
-
-    # func create fields
 
 
 if __name__ == '__main__':
--- test_tmp.py
+++ test_tmp.py
@@ -1,15 +1,71 @@
 import random
+import json
 
 from string import digits
 from faker import Faker
 from conf import MODEL
 
 INPUT_FILE = "books.txt"
+OUTPUT_FILE = "books_JSON_tmp.json"
 
 
-#-------------functions for Fields-------------------------------------------------
-    # 3.1 func random read one book from file book.txt
+def main() -> json:
+
+    while True:
+        try:
+            n = int(input("Введите стартовое число отсчета от 1 до 100 (по умолчанию 1): "))
+            if 1 <= n <= 100:
+                break
+            else:
+                print(f"Вы ввели чмсло не из требуемого диапазона, попробуйте снова.")
+                continue
+        except ValueError:
+            print("Вы ввели не число, попробуйте снова.")
+
+    start_pk = 1                            # начальное значение счетчика
+    if n >= start_pk:
+        start_pk = n
+        numbers_of_models = 10              # число выгружаемых записей
+        result_list = []                    # выходной список в json
+
+        # with open(OUTPUT_FILE, "w") as f:
+        #
+        #     for i in range(start_pk, numbers_of_models+1):
+        #         k_ = next(generator_library(i))
+        #         print(k_)
+        #         result_list.append(k_)
+        #         print(result_list)
+        #         json.dump(result_list, f, indent=4, ensure_ascii=False)
+
+        for number_ in range(start_pk, numbers_of_models+1):
+            k_ = next(generator_library(number_))
+            result_list.append(k_)
+
+        with open(OUTPUT_FILE, "w") as f:
+            json.dump(result_list, f, indent=4, ensure_ascii=False)
+
+
+def generator_library(count_ = 1) -> dict:
+
+    all_model_ = {
+        "model": MODEL,
+        "pk": count_,
+        "fields": {
+            "title": read_random_bookname_fromfile(),
+            "year": generate_random_year(),
+            "pages": generate_random_pages(),
+            "isbn13": generate_random_isbn13(),
+            "rating": generate_random_rating(),
+            "price": generate_random_price(),
+            "author": generate_random_author()
+        }
+    }
+
+    yield all_model_
+
+
 def read_random_bookname_fromfile() -> str:
+
     with open(INPUT_FILE, "r", encoding="UTF8") as f:
         rows_ = f.readlines()
         rows_ = [x.strip() for x in rows_]
@@ -18,65 +74,43 @@ def read_random_bookname_fromfile() -> str:
     return rows_random
 
 
-    # 3.2 func  random generate fictional year
-def generate_random_year(lenght_= 4) -> int:
-    tmp_year = random.sample(digits, lenght_)
-    tmp_year_str = "".join(tmp_year)
-
-    return int(tmp_year_str)
+def generate_random_year() -> int:
+    tmp_year = random.randint(1800, 2100)
+    return tmp_year
 
 
-    # 3.3 func  random generate pages
-def generate_random_pages(lenght_= 3) -> int:
-    tmp_pages = random.sample(digits, lenght_)
-    tmp_pages_str = "".join(tmp_pages)
-
-    return int(tmp_pages_str)
+def generate_random_pages() -> int:
+    tmp_pages = random.randint(100, 1000)
+    return tmp_pages
 
 
-    # 3.4 func generate random isbn13
 def generate_random_isbn13() -> str:
     fake = Faker()
     isbn13_ = fake.isbn13()
-
     return isbn13_
 
 
-    # 3.5 func random generate raiting (float)
 def generate_random_rating() -> float:
-        #tmp_taring = random.randint(0, 5) + random.random()
     tmp_rating = round(random.uniform(0, 5), 7)
-
     return tmp_rating
 
 
-    # 3.6 func random generate price(float)
 def generate_random_price() -> float:
-        #tmp_taring = random.randint(0, 5) + random.random()
     tmp_price = round(random.uniform(100, 2000), 2)
-
     return tmp_price
 
 
-    # 3.7 func random generate from 1 to 3 authors
 def generate_random_author() -> list:
     name_author = []
     count_ = random.randint(1, 3)
     for i in range(count_):
-            fake = Faker()
-            name_author.append(fake.name())
+        fake = Faker()
+        name_author.append(fake.name())
 
     return name_author
 
-    # func create fields
-fields_dict_ = {
-    "title": read_random_bookname_fromfile(),
-    "year": generate_random_year(),
-    "pages": generate_random_pages(),
-    "isbn13": generate_random_isbn13(),
-    "rating": generate_random_rating(),
-    "price": generate_random_price(),
-    "author": generate_random_author()
-}
 
-print(fields_dict_)
+if __name__ == '__main__':
+    main()
+
+
